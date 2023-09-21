7bf5bc17e9b1d6906b709568a7b883f12c681c8f
mohameddahy dahy <mohameddahy111@gmail.com>
2023-09-22 01:13:53 +0300

    fixed

--- src/middleware/valitetor.js
+++ src/middleware/valitetor.js
@@ -2,12 +2,13 @@ import { AppError } from "../utils/appError.js";
 
 export function validetor(schema) {
   return (req, res, next) => {
-    const data = { ...req.body, ...res.query, ...req.params };
-    let { errore } = schema.validate(data, { abortEarly: false });
-    if (!errore) {
+    console.log('hiii')
+    let data = {...req.body , ...req.query,  ...req.params }
+    let { error } = schema.validate(data, { abortEarly: false });
+    if (!error) {
       next();
     } else {
-      next(new AppError(`${errore.message}`, 400));
+      next(new AppError(`${error.message}`, 400));
     }
   };
 }
--- src/users/validtion/user.validtion.js
+++ src/users/validtion/user.validtion.js
@@ -1,14 +1,16 @@
 import  joi from 'joi';
 
 
-export const adduserValidation  =joi.object({
+ const adduserValidation  =joi.object({
   name: joi.string().min(2).required(),
   email:joi.string().email().required(),
   password: joi.string().min(6).max(20).required(),
-  phone:joi.number().min(11).max(13).required()
+  phone:joi.number().min(11).required()
 }).options({allowUnknown : true});
 
-export const loginValidation =joi.object({
+ const loginValidation =joi.object({
   email:joi.string().email().required(),
   password: joi.string().min(6).max(20).required(),
 }).options({allowUnknown : true});
+
+export { loginValidation, adduserValidation}
