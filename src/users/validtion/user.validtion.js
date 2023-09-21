4eea247ba16b953872cc668289205756afbe5b3e
mohameddahy dahy <mohameddahy111@gmail.com>
2023-09-22 00:50:45 +0300

    update joi

--- src/users/validtion/user.validtion.js
+++ src/users/validtion/user.validtion.js
@@ -1,8 +1,8 @@
-import * as joi from 'joi';
+import  joi from 'joi';
 
 
 export const adduserValidation  =joi.object({
-  name:joi.string().min(2).required(),
+  name: joi.string().min(2).required(),
   email:joi.string().email().required(),
   password: joi.string().min(6).max(20).required(),
   phone:joi.number().min(11).max(13).required()
