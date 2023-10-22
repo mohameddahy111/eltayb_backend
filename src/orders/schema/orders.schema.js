35f45ef9026e220bff82a2bc7836b8851846a2fe
mohameddahy dahy <mohameddahy111@gmail.com>
2023-10-22 20:43:55 +0300

    orders_fixd

--- src/orders/schema/orders.schema.js
+++ src/orders/schema/orders.schema.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const ordersShema = new mongoose.Schema(
   {
-    couponCode : {type:String},
+    couponCode: { type: String },
     userId: { type: mongoose.Types.ObjectId, ref: "User" },
     cartId: { type: mongoose.Types.ObjectId, ref: "Cart" },
-    payment_Mathed: { type: String ,enums:['credit' , "cash"]},
-    order_state: { type: String , default:'prepar'  },
+    payment_Mathed: { type: String, enums: ["credit", "cash"] },
+    order_state: { type: String, default: "prepar" },
     pay_state: { type: Boolean, default: false },
     shippingAddress: {
       city: { type: String },
@@ -18,13 +18,13 @@ const ordersShema = new mongoose.Schema(
         quantity: { type: Number },
         price: { type: Number },
         final_price: { type: Number },
-
+        size: { type: String },
       },
     ],
 
     payDate: { type: Date },
-    totlaPrice : { type: Number, required:true},
-    orgenalPrice: { type: Number, required:true}
+    totlaPrice: { type: Number, required: true },
+    orgenalPrice: { type: Number, required: true },
   },
   { timestamps: true }
 );
