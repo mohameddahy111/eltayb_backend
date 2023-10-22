bf5ca7633a59c3e052e36d2aa9380442da315766
mohameddahy dahy <mohameddahy111@gmail.com>
2023-10-22 23:56:20 +0300

    last update

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import productRouter from './src/product/router/product.router.js';
 import couponRouter from './src/coupon/router/coupon.router.js'
 import cartRouter from './src/cart/router/cart.router.js';
 import orderRouter from './src/orders/router/orders.router.js';
+import wishListRouter from './src/wishList/router/wishList.router.js';
 import cors from 'cors';
 
 dotenv.config()
@@ -21,6 +22,7 @@ app.use('/product/' , productRouter)
 app.use('/coupon/' ,couponRouter)
 app.use('/cart/' ,cartRouter)
 app.use('/orders/' ,orderRouter)
+app.use('/wishList/' ,wishListRouter)
 
 app.use((err , req, res ,next)=>{
   res.status(err.status || 400).send(err.message)
--- src/wishList/router/wishList.router.js
+++ src/wishList/router/wishList.router.js
@@ -1,6 +1,9 @@
 import express  from "express"
+import { addToWishList } from "../controller/wishList.controller.js"
+import {auth} from '../../middleware/auth.js'
+
 
 
 const router = express.Router()
-router.post('/' , auth , )
+router.post('/' , auth , addToWishList)
 export default router
