d644d9c1e2c6290e2adcc933ceb59bb206aecf7b
mohameddahy dahy <mohameddahy111@gmail.com>
2023-10-23 20:27:38 +0300

    w-u-5

--- src/wishList/schema/wishList.schema.js
+++ src/wishList/schema/wishList.schema.js
@@ -2,11 +2,7 @@ import mongoose from"mongoose";
 
 const wishList = new mongoose.Schema({
   userId :{type  : mongoose.Types.ObjectId  , ref :'User'},
-  products :[
-    {
-      productId :{type :mongoose.Types.ObjectId , ref :'Product'}
-    }
-  ]
+  products :{type :Array}
 })
 
 const List  = mongoose.model('WishList', wishList)
