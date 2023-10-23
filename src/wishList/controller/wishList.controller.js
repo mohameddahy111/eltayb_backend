27790ca1280d63ae448e49eb5b8cc9d4510770f2
mohameddahy dahy <mohameddahy111@gmail.com>
2023-10-23 20:08:07 +0300

    w_u_3

--- src/wishList/controller/wishList.controller.js
+++ src/wishList/controller/wishList.controller.js
@@ -8,12 +8,17 @@ export const addToWishList = errorHandler(async (req, res, next) => {
   if (user) {
     const product = user.products.find((x)=>x.productId == id)
     if (product) {
-    const productsList = user.products.filter((x)=>x.productId != id)
-      await List.findByIdAndUpdate(req.userId, {products :productsList })
+    // const productsList = user.products.filter((x)=>x.productId != id)
+      // await List.findByIdAndUpdate(req.userId, {products :productsList })
+      List.products.pull({productId : id})
+      await List.save()
       res.status(209).send({ message: "Success Remove item from Wish List" });
     } else {
-     const productsList = user.products.push(id)
-      await List.findByIdAndUpdate(req.userId, {products :productsList })
+      List.products.push({productId : id})
+      await List.save()
+
+    //  const productsList = user.products.push(id)
+    //   await List.findByIdAndUpdate(req.userId, {products :productsList })
       res.status(200).send({ message: "Success Add item to Wish List" });
       
     }
