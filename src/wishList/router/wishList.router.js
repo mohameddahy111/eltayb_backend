4759a393479aa9e7b580cad9d58269508f329a89
mohameddahy dahy <mohameddahy111@gmail.com>
2023-10-23 19:43:51 +0300

    update

--- src/wishList/router/wishList.router.js
+++ src/wishList/router/wishList.router.js
@@ -1,9 +1,10 @@
 import express  from "express"
-import { addToWishList } from "../controller/wishList.controller.js"
+import { addToWishList, getWishList } from "../controller/wishList.controller.js"
 import {auth} from '../../middleware/auth.js'
 
 
 
 const router = express.Router()
 router.post('/' , auth , addToWishList)
+router.get('/' , auth , getWishList)
 export default router
